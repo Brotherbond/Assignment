9579f642d31fc568ec9746a376746e1924a59895
Brotherbond <ehinmitankehinde@gmail.com>
2021-11-11 05:21:14 +0100

    nn having issue

--- 3/3.py
+++ 3/3.py
@@ -10,6 +10,7 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt # MATLAB-like way of plotting
 import seaborn as sns
+from scipy.optimize import minimize
 
 
 '''Data preprocessing: Loading & Cleaning
@@ -42,7 +43,7 @@ Y = nba_rookie_data.iloc[:, -1].values
 
 # Split dataset into train and test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.90,random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.40,random_state=0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -82,3 +83,122 @@ Y_pred_nb = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm_nb = confusion_matrix(Y_test, Y_pred_nb)
 
+
+
+# Using Neural network to  Dataset
+#using classes
+
+class Neural_Network(object):
+    def __init__(self, Lambda=0):        
+        #Define Hyperparameters
+        self.inputLayerSize = 19
+        self.outputLayerSize = 1
+        self.hiddenLayerSize = 19
+        
+        #Weights (parameters)
+        self.W1 = np.random.randn(self.inputLayerSize,self.hiddenLayerSize)
+        self.W2 = np.random.randn(self.hiddenLayerSize,self.outputLayerSize)
+        
+        #Regularization Parameter:
+        self.Lambda = Lambda
+        
+    def forward(self, X):
+        #Propogate inputs though network
+        self.z2 = np.dot(X, self.W1)
+        self.a2 = self.sigmoid(self.z2)
+        self.z3 = np.dot(self.a2, self.W2)
+        yHat = self.sigmoid(self.z3) 
+        return yHat
+        
+    def sigmoid(self, z):
+        #Apply sigmoid activation function to scalar, vector, or matrix
+        return 1/(1+np.exp(-z))
+    
+    def sigmoidPrime(self,z):
+        #Gradient of sigmoid
+        return np.exp(-z)/((1+np.exp(-z))**2)
+    
+    def costFunction(self, X, y):
+        #Compute cost for given X,y, use weights already stored in class.
+        self.yHat = self.forward(X)
+        J = 0.5*sum((y-self.yHat)**2)/X.shape[0] + (self.Lambda/2)*(np.sum(self.W1**2)+np.sum(self.W2**2))
+        return J
+        
+    def costFunctionPrime(self, X, y):
+        #Compute derivative with respect to W and W2 for a given X and y:
+        self.yHat = self.forward(X)
+        
+        delta3 = np.multiply(-(y-self.yHat), self.sigmoidPrime(self.z3))
+        #Add gradient of regularization term:
+        dJdW2 = np.dot(self.a2.T, delta3)/X.shape[0] + self.Lambda*self.W2
+        
+        delta2 = np.dot(delta3, self.W2.T)*self.sigmoidPrime(self.z2)
+        #Add gradient of regularization term:
+        dJdW1 = np.dot(X.T, delta2)/X.shape[0] + self.Lambda*self.W1
+        
+        return dJdW1, dJdW2
+    
+    #Helper functions for interacting with other methods/classes
+    def getParams(self):
+        #Get W1 and W2 Rolled into vector:
+        params = np.concatenate((self.W1.ravel(), self.W2.ravel()))
+        return params
+    
+    def setParams(self, params):
+        #Set W1 and W2 using single parameter vector:
+        W1_start = 0
+        W1_end = self.hiddenLayerSize*self.inputLayerSize
+        self.W1 = np.reshape(params[W1_start:W1_end], \
+                             (self.inputLayerSize, self.hiddenLayerSize))
+        W2_end = W1_end + self.hiddenLayerSize*self.outputLayerSize
+        self.W2 = np.reshape(params[W1_end:W2_end], \
+                             (self.hiddenLayerSize, self.outputLayerSize))
+        
+    def computeGradients(self, X, y):
+        dJdW1, dJdW2 = self.costFunctionPrime(X, y)
+        return np.concatenate((dJdW1.ravel(), dJdW2.ravel()))
+    
+    
+##Need to modify trainer class a bit to check testing error during training:
+class trainer(object):
+    def __init__(self, N):
+        #Make Local reference to network:
+        self.N = N
+        
+    def callbackF(self, params):
+        self.N.setParams(params)
+        self.J.append(self.N.costFunction(self.X, self.y))
+        self.testJ.append(self.N.costFunction(self.testX, self.testY))
+        
+    def costFunctionWrapper(self, params, X, y):
+        self.N.setParams(params)
+        cost = self.N.costFunction(X, y)
+        grad = self.N.computeGradients(X,y)
+        
+        return cost, grad
+        
+    def train(self, trainX, testX, trainY, testY):
+        #Make an internal variable for the callback function:
+        self.X = trainX
+        self.y = trainY
+        
+        self.testX = testX
+        self.testY = testY
+
+        #Make empty list to store training costs:
+        self.J = []
+        self.testJ = []
+        
+        params0 = self.N.getParams()
+
+        options = {'maxiter': 200, 'disp' : True}
+        _res = minimize(self.costFunctionWrapper, params0, jac=True, method='BFGS', \
+                                 args=(trainX, trainY), options=options, callback=self.callbackF)
+
+        self.N.setParams(_res.x)
+        self.optimizationResults = _res
+        
+        
+NN = Neural_Network(Lambda=0.0001)        
+T = trainer(NN)
+T.train(X_train, X_test, Y_train, Y_test )
